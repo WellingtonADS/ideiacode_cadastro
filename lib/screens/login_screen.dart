2d258db036267dd86984aae9551d3a908f2d4693
WellingtonAds <ads231n2@gmail.com>
2024-06-26 18:01:22 -0400

    modified:   lib/screens/login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,6 +12,7 @@ class LoginScreen extends StatefulWidget {
 class LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   String? _emailErrorText;
+  String? _passwordErrorText;
 
   void _validateEmail(String value) {
     setState(() {
@@ -25,12 +26,21 @@ class LoginScreenState extends State<LoginScreen> {
     });
   }
 
+  void _validatePassword(String value) {
+    setState(() {
+      if (value.isEmpty) {
+        _passwordErrorText = 'Por favor, insira uma senha';
+      } else {
+        _passwordErrorText = null;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
-        // Utilizando o estilo de texto do app_styles.dart para o título da AppBar
+        title: const Text('Login', style: AppStyles.titleTextStyle), // Utilizando o estilo de texto do título da AppBar
       ),
       body: Center(
         child: Container(
@@ -41,37 +51,48 @@ class LoginScreenState extends State<LoginScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextField(
+                TextFormField(
                   decoration: InputDecoration(
                     labelText: 'E-mail',
                     labelStyle: AppStyles.subtitleTextStyle,
                     errorText: _emailErrorText,
                   ),
                   onChanged: _validateEmail,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira um e-mail';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 20),
-                const SizedBox(
-                  width: 600,
-                  child: TextField(
-                    key: Key('passwordField'),
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      labelStyle: AppStyles.subtitleTextStyle,
-                    ),
-                    obscureText: true,
+                TextFormField(
+                  key: const Key('passwordField'),
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: AppStyles.subtitleTextStyle,
+                    errorText: _passwordErrorText,
                   ),
+                  obscureText: true,
+                  onChanged: _validatePassword,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira uma senha';
+                    }
+                    return null;
+                  },
                 ),
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.pushReplacementNamed(context, Routes.homeScreen);
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushReplacementNamed(context, Routes.homeScreen);
+                    }
                   },
-                  style: AppStyles.elevatedButtonStyle,
-                  // Utilizando o estilo de botão do app_styles.dart
+                  style: AppStyles.elevatedButtonStyle, // Utilizando o estilo de botão do app_styles.dart
                   child: const Text(
                     'Login',
-                    style: AppStyles.buttonTextStyle,
-                    // Utilizando o estilo de texto do botão do app_styles.dart
+                    style: AppStyles.buttonTextStyle, // Utilizando o estilo de texto do botão do app_styles.dart
                   ),
                 ),
               ],
