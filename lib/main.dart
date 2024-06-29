d688a34d23e9fab28b80eebdf8b70aaa4b005a8b
WellingtonAds <ads231n2@gmail.com>
2024-06-29 10:54:42 -0400

    Changes to be committed: 	modified:   lib/main.dart 	modified:   lib/services/auth_service.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:ideiacode_cadastro/Styles/app_styles.dart';
 import 'package:ideiacode_cadastro/routing/routes.dart';
-
-
+import 'package:provider/provider.dart';
+import 'package:ideiacode_cadastro/services/auth_service.dart';
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Cadastro());
-  
 }
 
 class Cadastro extends StatelessWidget {
@@ -21,15 +19,21 @@ class Cadastro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'IdeiaCode Cadastro',
-      theme: ThemeData(
-        primaryColor: AppStyles.primaryColor,
-        appBarTheme: AppStyles.appBarTheme, // Aplicando o tema da AppBar globalmente
-
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: AuthService(), // Use ChangeNotifierProvider.value instead of ChangeNotifierProvider
+        ),
+      ],
+      child: MaterialApp(
+        title: 'IdeiaCode Cadastro',
+        theme: ThemeData(
+          primaryColor: AppStyles.primaryColor,
+          appBarTheme: AppStyles.appBarTheme, // Aplicando o tema da AppBar globalmente
+        ),
+        initialRoute: Routes.login,
+        routes: Routes.getRoutes(),
       ),
-      initialRoute: Routes.login,
-      routes: Routes.getRoutes(),
     );
   }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -15,47 +15,56 @@ class AuthService extends ChangeNotifier {
     _authCheck();
   }
 
-  _authCheck() {
+  // Verifica o estado de autenticação inicial
+  void _authCheck() {
     _auth.authStateChanges().listen((User? user) {
-      usuario = (user == null) ? null : user;
+      usuario = user;
       isLoading = false;
       notifyListeners();
     });
   }
 
-  _getUser() {
+  // Obtém o usuário atual
+  void _getUser() {
     usuario = _auth.currentUser;
     notifyListeners();
   }
 
-  registrar(String email, String senha) async {
+  // Registra um novo usuário
+  Future<void> registrar(String email, String senha) async {
     try {
       await _auth.createUserWithEmailAndPassword(email: email, password: senha);
-      _getUser();
+      _getUser(); // Atualiza o usuário após o registro
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         throw AuthException('A senha é muito fraca!');
       } else if (e.code == 'email-already-in-use') {
         throw AuthException('Este email já está cadastrado');
+      } else {
+        throw AuthException('Erro ao registrar: ${e.message}');
       }
     }
   }
 
-  login(String email, String senha) async {
+  // Faz login com email e senha
+  Future<void> login(String email, String senha) async {
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: senha);
-      _getUser();
+      _getUser(); // Atualiza o usuário após o login
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         throw AuthException('Email não encontrado. Cadastre-se.');
       } else if (e.code == 'wrong-password') {
         throw AuthException('Senha incorreta. Tente novamente');
+      } else {
+        throw AuthException('Erro ao fazer login: ${e.message}');
       }
     }
   }
 
-  logout() async {
+  // Faz logout
+  Future<void> logout() async {
     await _auth.signOut();
-    _getUser();
+    _getUser(); // Atualiza o usuário após o logout
   }
 }
